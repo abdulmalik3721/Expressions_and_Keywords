f02f5cc8b6e2cc5d631bace3ae7cb4f67762bb60
ABDUL MALIK UNEEBA <malik.kasganj@gmail.com>
2024-09-09 09:14:37 +0530

    name added

--- src/KeyWords.java
+++ src/KeyWords.java
@@ -4,6 +4,7 @@ public class KeyWords {
         int b =12; // the entire line is a statement
         double d = a/b;
         System.out.println("the result is: " + d);
+        System.out.println("ABDUL MALIK UNEEBA");
 
     }
 }
