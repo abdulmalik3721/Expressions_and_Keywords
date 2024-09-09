1344cc1a56650e3b796fb22e2a66f455ad046080
ABDUL MALIK UNEEBA <malik.kasganj@gmail.com>
2024-09-09 09:11:00 +0530

    i added 'is' in

--- src/KeyWords.java
+++ src/KeyWords.java
@@ -3,6 +3,7 @@ public class KeyWords {
         int a = 50; // a=50- is an expression excluding semi-colon(;)
         int b =12; // the entire line is a statement
         double d = a/b;
-        System.out.println("the result: " + d);
+        System.out.println("the result is: " + d);
+
     }
 }
