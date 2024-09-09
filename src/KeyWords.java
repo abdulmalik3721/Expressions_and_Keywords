7e1f4d972a63d466283d083516454f06dc76bfea
zamiul haq <zamilul.haqKsg@gmail.com>
2024-09-09 09:05:38 +0530

    example of expression and statements added

---
+++ src/KeyWords.java
@@ -0,0 +1,8 @@
+public class KeyWords {
+    public static void main(String[] args) {
+        int a = 50; // a=50- is an expression excluding semi-colon(;)
+        int b =12; // the entire line is a statement
+        double d = a/b;
+        System.out.println("the result: " + d);
+    }
+}
